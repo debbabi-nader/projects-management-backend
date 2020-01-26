fe1ea23e6d638016181103a4c1bb3478dab30e47
Nader Debbabi <nader.debbabi.30@gmail.com>
2020-01-26 14:23:31 +0100

    Initial commit

---
+++ src/main/java/io/isamm/projectsmanagement/ProjectsManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package io.isamm.projectsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/isamm/projectsmanagement/ProjectsManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.isamm.projectsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
