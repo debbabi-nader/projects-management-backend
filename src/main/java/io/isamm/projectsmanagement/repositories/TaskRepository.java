b856467cf9d930997c2a0793254a9187d1945b3f
Nader Debbabi <nader.debbabi.30@gmail.com>
2020-01-31 16:12:13 +0100

    Fixed delete tasks bug

--- src/main/java/io/isamm/projectsmanagement/repositories/TaskRepository.java
+++ src/main/java/io/isamm/projectsmanagement/repositories/TaskRepository.java
@@ -3,6 +3,7 @@ package io.isamm.projectsmanagement.repositories;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,7 @@ public interface TaskRepository extends JpaRepository<TaskEntity, String> {
 	@Query("SELECT task FROM TaskEntity AS task WHERE task.project.id LIKE :id ORDER BY task.createdAt DESC")
 	public Collection<TaskEntity> getTasksByProjectId(@Param("id") String id);
 	
+	@Modifying
 	@Query("DELETE FROM TaskEntity AS task WHERE task.project.id LIKE :id")
 	public void deleteTasksByProjectId(@Param("id") String id);
 
