33c9fd94982f86829605fd80da0417c33e1e683c
Nader Debbabi <nader.debbabi.30@gmail.com>
2020-02-01 10:43:18 +0100

    Added get users endpoint

--- src/main/java/io/isamm/projectsmanagement/controllers/UserController.java
+++ src/main/java/io/isamm/projectsmanagement/controllers/UserController.java
@@ -1,6 +1,9 @@
 package io.isamm.projectsmanagement.controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +28,11 @@ public class UserController {
 	@RequestMapping(value = "", params = { "email" }, method = RequestMethod.GET)
 	public UserEntity getUserByEmail(@RequestParam(value = "email", required = true) String email) {
 		return this.userService.getUserByEmail(email);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public Collection<UserEntity> getUsers() {
+		return this.userService.findAll(Sort.by(Sort.Direction.ASC, "firstName"));
 	}
 	
 }
